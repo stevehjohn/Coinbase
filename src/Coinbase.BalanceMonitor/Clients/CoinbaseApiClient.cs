a928397c19e8ad2524fad1aad6638640e16d0ed0
Stevö <stevehuwjohn@gmail.com>
2021-01-25 19:41:00 +0000

    Signature working

--- src/Coinbase.BalanceMonitor/Clients/CoinbaseApiClient.cs
+++ src/Coinbase.BalanceMonitor/Clients/CoinbaseApiClient.cs
@@ -17,15 +17,17 @@ namespace Coinbase.BalanceMonitor.Clients
                       {
                           BaseAddress = new Uri(AppSettings.Instance.ApiUri)
                       };
+
+            _client.DefaultRequestHeaders.Add("CB-ACCESS-KEY", AppSettings.Instance.ApiKey);
         }
 
         public async Task<int> GetAccountBalance()
         {
             var random = new Random();
 
-            var message = new HttpRequestMessage(HttpMethod.Get, "/v2/accounts");
+            var message = new HttpRequestMessage(HttpMethod.Get, "/v2/accounts?order=asc");
 
-            AddRequestHeaders(message, string.Empty);
+            AddRequestHeaders(message);
 
             var response = await _client.SendAsync(message);
 
@@ -34,24 +36,23 @@ namespace Coinbase.BalanceMonitor.Clients
             return random.Next(10000);
         }
 
-        private void AddRequestHeaders(HttpRequestMessage message, string body)
+        private void AddRequestHeaders(HttpRequestMessage message, string body = null)
         {
-            _client.DefaultRequestHeaders.Clear();
-
             var timestamp = $"{(long) DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds}";
 
             // ReSharper disable once PossibleNullReferenceException
-            var toSign = $"{timestamp}{message.Method.ToString().ToUpper()}{message.RequestUri.OriginalString}{body}";
+            var toSign = $"{timestamp}{message.Method.ToString().ToUpper()}{message.RequestUri.OriginalString}{body ?? string.Empty}";
 
             var bytes = Encoding.ASCII.GetBytes(toSign);
 
-            using var hmacsha256 = new HMACSHA256(Convert.FromBase64String(AppSettings.Instance.ApiSecret));
+            //using var hmacsha256 = new HMACSHA256(Convert.FromBase64String(AppSettings.Instance.ApiSecret));
+            using var hmacsha256 = new HMACSHA256(Encoding.UTF8.GetBytes(AppSettings.Instance.ApiSecret));
 
             var hash = hmacsha256.ComputeHash(bytes);
 
-            _client.DefaultRequestHeaders.Add("CB-ACCESS-KEY", AppSettings.Instance.ApiKey);
-            _client.DefaultRequestHeaders.Add("CB-ACCESS-SIGN", Convert.ToBase64String(hash));
-            _client.DefaultRequestHeaders.Add("CB-ACCESS-TIMESTAMP", timestamp);
+            message.Headers.Add("CB-ACCESS-SIGN", BitConverter.ToString(hash).Replace("-", string.Empty).ToLower());
+            //message.Headers.Add("CB-ACCESS-SIGN", Convert.ToBase64String(hash));
+            message.Headers.Add("CB-ACCESS-TIMESTAMP", timestamp);
         }
     }
 }
